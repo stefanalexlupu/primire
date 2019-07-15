b844cdb0ac8101d966dacd0a26a80c4dfcefdf85
Lupu Alexandru Stefan <alex.loopu@gmail.com>
2019-07-15 23:10:27 +0300

    Separate firebase config (private data)

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,17 +1,8 @@
 import firebase from 'firebase/app';
+import firebaseConfig from './firebaseConfig';
 import 'firebase/firestore';
 import 'firebase/messaging';
 import 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDk-Oux0Mu9Oq9I8FJrSLy0MgQ26gelNFw',
-  authDomain: 'primire-web-app.firebaseapp.com',
-  databaseURL: 'https://primire-web-app.firebaseio.com',
-  projectId: 'primire-web-app',
-  storageBucket: '',
-  messagingSenderId: '728389655018',
-  appId: '1:728389655018:web:6509d21902264ac2',
-};
 
 firebase.initializeApp(firebaseConfig);
 
